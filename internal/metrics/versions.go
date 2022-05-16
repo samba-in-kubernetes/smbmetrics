74023b42a3c822f05a8d0c25038f1288c5c6c392
Shachar Sharon <ssharon@redhat.com>
2022-05-16 12:25:00 +0300

    metrics: resolve versions in single module
    Resolve various versions info within single module.
    
    Signed-off-by: Shachar Sharon <ssharon@redhat.com>

---
+++ internal/metrics/versions.go
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package metrics
+
+import (
+	"context"
+)
+
+var (
+	defaultVersions Versions
+)
+
+type Versions struct {
+	Version      string
+	CommitID     string
+	SambaImage   string
+	SambaVersion string
+	CtdbVersion  string
+}
+
+// UpdateDefaultVersions assigns defaults upon init
+func UpdateDefaultVersions(version, commitid string) {
+	defaultVersions.Version = version
+	defaultVersions.CommitID = commitid
+}
+
+// ResolveVersions is a best-effort to resolve current pod's versions info
+func ResolveVersions(clnt *kclient) (Versions, error) {
+	var err error
+	vers := Versions{
+		Version:  defaultVersions.Version,
+		CommitID: defaultVersions.CommitID,
+	}
+	if clnt != nil {
+		vers.SambaImage, err = resolveSambaImage(clnt)
+		if err != nil {
+			return vers, err
+		}
+	}
+	vers.SambaVersion, err = resolveSambaVersion()
+	if err != nil {
+		return vers, err
+	}
+	vers.CtdbVersion, err = resolveCtdbVersion()
+	if err != nil {
+		return vers, err
+	}
+	return vers, nil
+}
+
+func resolveSambaImage(clnt *kclient) (string, error) {
+	pod, err := GetSelfPod(context.TODO(), clnt)
+	if err != nil {
+		return "", err
+	}
+	for _, cont := range pod.Spec.Containers {
+		if cont.Name == "samba" {
+			return cont.Image, nil
+		}
+	}
+	return "", nil
+}
+
+func resolveSambaVersion() (string, error) {
+	return executeRpmQCommand("samba")
+}
+
+func resolveCtdbVersion() (string, error) {
+	return executeRpmQCommand("ctdb")
+}
+
+func executeRpmQCommand(name string) (string, error) {
+	return executeCommand("rpm", "-q", name)
+}
