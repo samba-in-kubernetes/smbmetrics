52293f0d95666a0aed58cad9987f186be6d76569
Shachar Sharon <ssharon@redhat.com>
2022-05-16 12:34:36 +0300

    metrics: export versions as Prometheus metrics
    Extern various versions info as labels over dedicated Prometheus metrics
    called 'smb_metrics_status'.
    
    Signed-off-by: Shachar Sharon <ssharon@redhat.com>

--- internal/metrics/collectors.go
+++ internal/metrics/collectors.go
@@ -12,6 +12,7 @@ var (
 
 func (sme *smbMetricsExporter) register() error {
 	cols := []prometheus.Collector{
+		sme.newSmbVersionsCollector(),
 		sme.newSmbSharesCollector(),
 		sme.newSmbLocksCollector(),
 	}
@@ -34,6 +35,48 @@ func (col *smbCollector) Describe(ch chan<- *prometheus.Desc) {
 	for _, d := range col.dsc {
 		ch <- d
 	}
+}
+
+type smbVersionsCollector struct {
+	smbCollector
+	clnt *kclient
+}
+
+func (col *smbVersionsCollector) Collect(ch chan<- prometheus.Metric) {
+	status := 0
+	vers, err := ResolveVersions(col.clnt)
+	if err != nil {
+		status = 1
+	}
+	ch <- prometheus.MustNewConstMetric(
+		col.dsc[0],
+		prometheus.GaugeValue,
+		float64(status),
+		vers.Version,
+		vers.CommitID,
+		vers.SambaImage,
+		vers.SambaVersion,
+		vers.CtdbVersion,
+	)
+}
+
+func (sme *smbMetricsExporter) newSmbVersionsCollector() prometheus.Collector {
+	col := &smbVersionsCollector{}
+	col.sme = sme
+	col.clnt, _ = newKClient()
+	col.dsc = []*prometheus.Desc{
+		prometheus.NewDesc(
+			collectorName("metrics", "status"),
+			"Current metrics-collector status versions",
+			[]string{
+				"version",
+				"commitid",
+				"sambaimage",
+				"sambavers",
+				"ctdbvers",
+			}, nil),
+	}
+	return col
 }
 
 type smbSharesCollector struct {
