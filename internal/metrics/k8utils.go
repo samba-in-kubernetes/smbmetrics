00b05ced746edc8ddc8a80329d7387154cd41ea1
Shachar Sharon <ssharon@redhat.com>
2022-06-06 09:18:31 +0300

    metrics: disable ci-lint warn in go1.17
    Ignore false warns in go1.17 which fails CI.
    
    Signed-off-by: Shachar Sharon <ssharon@redhat.com>

--- internal/metrics/k8utils.go
+++ internal/metrics/k8utils.go
@@ -10,7 +10,7 @@ import (
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/types" // nolint:typecheck
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
