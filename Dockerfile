samba-in-kubernetes/smbmetrics

commit 47959b5249d7a098e1cb17f498c0d9a80e192039

image: allow passing explicit samba base-image
The smbmetrics image is based on one of multiple samba-server images.
Allow user to define an explicit base-image, with latest as default.

Signed-off-by: Shachar Sharon <ssharon@redhat.com>

Dockerfile
@@ -1,8 +1,11 @@
-# Build smbmetrics
-FROM docker.io/golang:1.24 as builder
+# Args
+ARG BASE_IMG="quay.io/samba.org/samba-server:latest"
 ARG GIT_VERSION="(unset)"
 ARG COMMIT_ID="(unset)"
 ARG ARCH=""
+
+# Build smbmetrics
+FROM docker.io/golang:1.24 as builder
 
 WORKDIR /workspace
 # Copy the Go Modules manifests
@@ -23,7 +26,7 @@ RUN CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} GO111MODULE=on \
     -o smbmetrics cmd/main.go
 
 # Use samba-server (with its smb.conf and samba utils) as base image
-FROM quay.io/samba.org/samba-server
+FROM $BASE_IMG
 COPY --from=builder /workspace/smbmetrics /bin/smbmetrics
 
 ENTRYPOINT ["/bin/smbmetrics"]

commit 5fdcef823298f39d895e4f34f3852816d66342dd

dockerfile: fix wrong args order
Commit 47959b5249 (image: allow passing explicit samba base-image)
introduced the ability to set explicit base image and tag via command
line args. However, it also introduced a bug in the Dockerfile flow
which causes version and commit-id not to propagate properly into
smbmetrics binary.

Signed-off-by: Shachar Sharon <ssharon@redhat.com>

Dockerfile
@@ -1,11 +1,10 @@
-# Args
 ARG BASE_IMG="quay.io/samba.org/samba-server:latest"
-ARG GIT_VERSION="(unset)"
-ARG COMMIT_ID="(unset)"
-ARG ARCH=""
 
 # Build smbmetrics
 FROM docker.io/golang:1.24 AS builder
+ARG GIT_VERSION="(unset)"
+ARG COMMIT_ID="(unset)"
+ARG ARCH=""
 
 WORKDIR /workspace
 # Copy the Go Modules manifests